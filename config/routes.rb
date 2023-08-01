4b723645246c3f10788f9388c92d7008308fb7c7
gnmathew <genemathewdorado@gmail.com>
2023-08-01 13:52:35 +0800

    implemented routes constraint

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+  constraints(ClientDomainConstraint.new) do
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  constraints(AdminDomainConstraint.new) do
+
+  end
+
 end
