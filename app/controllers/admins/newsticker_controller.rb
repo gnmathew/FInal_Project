8a738a13ced31b03613edd0eda358cd568326d63
gnmathew <genemathewdorado@gmail.com>
2023-08-25 18:55:18 +0800

    [Feature 32] NewsTicker model - created CRUD features

---
+++ app/controllers/admins/newsticker_controller.rb
@@ -0,0 +1,54 @@
+class Admins::NewstickerController < Admins::BaseController
+  before_action :set_newsticker, except: [:index, :new, :create]
+  def index
+    @newstickers = Newsticker.all
+  end
+
+  def show
+  end
+
+  def new
+    @newsticker = Newsticker.new
+  end
+
+  def create
+    @newsticker = Newsticker.new(newsticker_params)
+    if @newsticker.save
+      flash[:notice] = "You have successfully created a NewsTicker"
+      redirect_to newsticker_index_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @newsticker.update(newsticker_params)
+      flash[:notice] = "You have successfully updated a NewsTicker"
+      redirect_to newsticker_index_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @newsticker.destroy
+      flash[:notice] = 'Deleted successfully'
+    else
+      flash[:alert] = 'Delete failed'
+    end
+    redirect_to newsticker_index_path
+  end
+
+  private
+
+  def set_newsticker
+    @newsticker = Newsticker.find(params[:id])
+  end
+
+  def newsticker_params
+    params.require(:newsticker).permit(:content, :status)
+  end
+end
--- app/models/newsticker.rb
+++ app/models/newsticker.rb
@@ -1,2 +1,7 @@
 class Newsticker < ApplicationRecord
+
+  belongs_to :admin, class_name: 'Newsticker', optional: true
+
+  enum status: {active: 0, inactive: 1}
+
 end
