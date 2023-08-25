fc102f21b4d0160a044a13257dc3d7aab9662cbc
gnmathew <genemathewdorado@gmail.com>
2023-08-25 19:43:06 +0800

    [Feature 33] generated Banner model with schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_25_104528) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_112710) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -64,6 +64,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_25_104528) do
     t.index ["address_province_id"], name: "index_addresses_on_address_province_id"
     t.index ["address_region_id"], name: "index_addresses_on_address_region_id"
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "banners", charset: "utf8mb4", force: :cascade do |t|
+    t.string "preview"
+    t.datetime "online_at"
+    t.datetime "offline_at"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "bets", charset: "utf8mb4", force: :cascade do |t|
