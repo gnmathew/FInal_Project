d3d357e9c07fa032c5e3b7ced94b8a5eb22dcda5
gnmathew <genemathewdorado@gmail.com>
2023-08-27 22:18:41 +0800

    [Feature 35] Implemented the Client Homepage view feature

--- app/controllers/clients/home_controller.rb
+++ app/controllers/clients/home_controller.rb
@@ -2,6 +2,8 @@ class Clients::HomeController < ApplicationController
   def index
     @banners = Banner.where(status: "active" ).where("created_at <= ?", Time.now).where("offline_at > ?", Time.now)
     @newstickers = Newsticker.where(status: "active")
+    @last_five_feedbacks = Winner.published.last(5)
+    @starting_items = Item.active.starting.first(8)
   end
 
 end
